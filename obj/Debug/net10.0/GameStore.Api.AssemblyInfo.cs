1bf901e4b7d2e261d16b82a3833713bbe873e4ab
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-27 16:31:28 -0600

    installed nuGet design package and prepared for migration.

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e1462de36015a135f71471630469177f437c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e826dc42cd0ccaaf27ea2550dcfe0a00cfe6a3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
