68717db677a2021ea7d7476b998b289fdf8862a4
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-27 17:13:36 -0600

    created the schema for my db and ran the update on EFC so that initial creates took place.

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e826dc42cd0ccaaf27ea2550dcfe0a00cfe6a3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf901e4b7d2e261d16b82a3833713bbe873e4ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
