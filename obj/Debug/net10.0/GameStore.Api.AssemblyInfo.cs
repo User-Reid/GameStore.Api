23cefa9d6867d8b7d93a83d1deefbd5c6f2ca26c
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-28 11:51:20 -0600

    applied our connection string to our appsettings.json for reusability and ease of access.

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e28c2f383046a303c551347da9719a267f73b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213c1095b65ecf47d2303cc29d471b803df53c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
